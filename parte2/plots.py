a4ee34708d1c5262e2129f44be24cd431b35b9f1
AZANK7173 <felipeazank@gmail.com>
2022-05-30 22:44:32 -0300

    add visualization and yes

---
+++ parte2/plots.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+import seaborn as sns
+import plotly.express as ex
+
+from cria_malha import *
+
+props_geo = {
+    "R_A" : [0.03, 0.08 + 0.03],
+    "R_B" : [0.03 + 0.02, 0.03 + 0.05],
+    "Theta_A" : [0, np.deg2rad(40)],
+    "Theta_B" : [0, np.deg2rad(18)],
+}
+
+
+def cria_plot(M, dr, dtheta):
+
+    sns.color_palette("tab10")
+
+    X = []
+    Y = []
+    Valores = []
+
+    for i in range(M.shape[0]): #raio
+        for j in range(M.shape[1]): #angulo
+            raio = 0.03 + i*dr
+            angulo = j*dtheta
+
+            x = raio*np.cos(np.deg2rad(angulo))
+            y = raio*np.sin(np.deg2rad(angulo))
+
+            valor = M[i,j]
+
+            X.append(x)
+            Y.append(y)
+            Valores.append(valor)
+    
+    for item in Valores:
+        item = str(Valores)
+    
+    fig = ex.scatter(x=X, y=Y, color=Valores)
+    fig.show()
--- parte2/resolve_potencial.py
+++ parte2/resolve_potencial.py
@@ -2,6 +2,7 @@ import numpy as np
 from coeficients import *
 from liebmann import *
 from cria_malha import *
+from plots import *
 
 
 def function(M, i, j):
@@ -15,3 +16,121 @@ def function(M, i, j):
     print(f"ADJ: {adjacentes}")
 
     return coef @ adjacentes
+
+def verifica_condicao(i, j, dr, dtheta):
+    #verifica em qual condicao esta o ponto i, j da malha
+    raio = i*dr + min(props_geo['R_A'])
+    theta = np.rad2deg(j*dtheta + min(props_geo['Theta_A']))
+
+    print(f"Raio = {raio}, Theta = {theta}")
+
+    if(theta >= 40): 
+        print("Borda superior do material A")
+
+        return 0 
+    
+    elif(theta == 0):
+        if(0.05 < raio < 0.08):
+            print("borda inferior do material B")
+
+            return 1 #falta
+
+        elif(raio == 0.05):
+            print("borda esquerda do material B")
+
+            return 2 
+        elif(raio == 0.08):
+            print("borda direita do material B")
+
+            return 3 #falta
+        elif(raio == 0.03):
+            print("borda esquerda do material A")
+
+            return 4 #ajuste 
+        elif(raio == 0.11):
+            print("borda direita do material A")
+
+            return 5 #ajuste 
+        else:
+            print("borda inferior do material A")
+
+            return 6 #falta
+
+    
+    elif(theta == 18):
+        if(0.05 < raio < 0.08):
+            print("borda superior do material B")
+
+            return 7 #AB byPhi
+            
+        if(raio == 0.11):
+            print("borda direita do material A")
+
+            return 5
+
+        elif(raio == 0.03):
+            print("borda esquerda do material A")
+
+            return 4
+        else:
+            print("interior do material A")
+
+            return 8 #interior A
+
+    elif(theta > 18):
+
+        if(raio == 0.11):
+            print("borda direita do material A")
+
+            return 5
+
+        elif(raio == 0.03):
+            print("borda esquerda do material A")
+
+            return 4
+
+        else:
+            print("interior do material A")
+
+            return 8 
+
+    else:
+        if(0.05 < raio < 0.08):
+            print("interior do material B")
+
+            return 9 #interior B 
+        elif(raio == 0.05):
+            print("borda esquerda do material B")
+
+            return 2
+        elif(raio == 0.08):
+            print("borda direita do material B")
+
+            return 3
+        elif(raio == 0.03):
+            print("borda esquerda do material A")
+
+            return 4 
+        elif(raio == 0.11):
+            print("borda direita do material A")
+
+            return 5
+        else:
+            print("interior do material A")
+
+            return 8
+
+if __name__ == "__main__":
+    dr = 0.001
+    dtheta = np.deg2rad(1)
+    M = cria_malha(dr, dtheta)
+
+    for i in range(M.shape[0]):
+        for j in range(M.shape[1]):
+            M[i,j]=verifica_condicao(i,j, dr, dtheta)
+
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    sns.color_palette("tab10")
+    cria_plot(M, dr, dtheta)
+
